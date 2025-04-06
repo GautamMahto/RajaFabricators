ddce3c699393d8a046441cfd3256711b72ab5f47
GautamMahto <gautammahtoaiie31aii.ac.in@gmail.com>
2025-04-06 19:47:21 +0530

    updated email port

--- email_sender.js
+++ email_sender.js
@@ -85,5 +85,5 @@ app.post("/send-email", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT_EMAIL || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
